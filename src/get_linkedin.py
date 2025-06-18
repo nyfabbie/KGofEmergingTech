64ee390ebe86f9110e728a587c927fee76f1ee93
nyfabbie <93514820+nyfabbie@users.noreply.github.com>
2025-06-18 02:29:29 +0200

    fetch dataset from kaggle without downloading and uploading csvs

--- src/get_linkedin.py
+++ src/get_linkedin.py
@@ -1,4 +1,6 @@
 from staffspy import LinkedInAccount
+import kagglehub
+from kagglehub import KaggleDatasetAdapter
 
 account = LinkedInAccount(
     # driver_type=DriverType( # if issues with webdriver, specify its exact location, download link in the FAQ
@@ -21,5 +23,23 @@ def fetch_linkedin(startup: str):
     staff = staff[['search_term', 'headline', 'current_position', 'skills']]
     staff = staff.rename(columns={'search_term': 'start_up'})
     return staff
+
+def fetch_kaggle():
+    jobs_skills = kagglehub.dataset_load(
+        KaggleDatasetAdapter.PANDAS,
+        "asaniczka/1-3m-linkedin-jobs-and-skills-2024?select=job_skills.csv",
+        "job_skills.csv",
+    )
+    jobs_skills = jobs_skills.set_index('job_link')
+    job_postings = kagglehub.dataset_load(
+        KaggleDatasetAdapter.PANDAS,
+        "asaniczka/1-3m-linkedin-jobs-and-skills-2024?select=linkedin_job_postings.csv",
+        "linkedin_job_postings.csv",
+        pandas_kwargs={"columns": ["job_link", "job_title"]}
+    )
+    job_postings = job_postings.set_index('job_link')
+    job_postings_skills = jobs_skills.join(job_postings, how='inner')
+    return job_postings_skills
+
 
 fetch_linkedin("Cofactor Genomics").to_csv('C:/Users/abbie/PycharmProjects/KGofEmergingTech/data/linkedin_staff.csv', index=False)
